b04f742376125eddb31519356646fdc974a87faf
ĐẠT LÊ THỌ <wearethesame13@gmail.com>
2021-09-20 00:09:18 +0700

    remove select

--- server.ts
+++ server.ts
@@ -1,12 +1,15 @@
+var fs = require("fs");
+var privateKey = fs.readFileSync("sslcert/server.key", "utf8");
+var certificate = fs.readFileSync("sslcert/server.crt", "utf8");
 const app = require("express")();
-const server = require("http").Server(app);
+const server = require("https").createServer(credentials, app);
 const io = require("socket.io")(server);
 const next = require("next");
 
 const dev = process.env.NODE_ENV !== "production";
 const nextApp = next({ dev });
 const nextHandler = nextApp.getRequestHandler();
-
+var credentials = { key: privateKey, cert: certificate };
 let port = 3000;
 
 nextApp.prepare().then(() => {
@@ -15,6 +18,6 @@ nextApp.prepare().then(() => {
   });
   server.listen(port, (err) => {
     if (err) throw err;
-    console.log(`> Ready on http://localhost:${port}`);
+    console.log(`> Ready on https://localhost:${port}`);
   });
 });
